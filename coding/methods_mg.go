1d26ec58bc920fa538e8f6c5fcdae817c6ae120b
martinaCas <martinacasal.web@gmail.com>
2022-04-05 18:00:21 +0200

    commit

--- coding/methods_mg.go
+++ coding/methods_mg.go
@@ -15,7 +15,7 @@ type User struct {
 	name    string
 	surname string
 	email   string
-	age     int
+	age     string
 	phone   string
 }
 
@@ -25,13 +25,28 @@ type User struct {
 */
 func main() {
 
-	var collection mongo.Collection = connect_toMongoDB()
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	//connect to db
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017/"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("SEI CONNESSO AL MONGODB")
+	fmt.Println()
+
+	ctx, _ := context.WithTimeout(context.Background(), 200*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//ricerca database esistente
+	usersDatabase := client.Database("user_db")
+	//new collection
+	usersCollection := usersDatabase.Collection("user_details")
 
 	fmt.Println("PER INSERIRE UN NUOVO UTENTE, PREMI 1\n" +
-		"PER MODIFICARE ETA' UTENTE, PREMI 2\n" +
-		"PER MODIFICARE NUMERO TELEFONO UTENTE, PRIMI 3\n" +
-		"PER ELIMINARE UN UTENTE, PREMI 4")
+		"PER MODIFICARE UTENTE, PREMI 2\n" +
+		"PER ELIMINARE UN UTENTE, PREMI 3")
 
 	var input int
 	fmt.Scanln(&input)
@@ -39,32 +54,79 @@ func main() {
 	switch input {
 	case 1:
 		var user_result User = insertNewUser()
-		fmt.Println("sono nel case 1")
+		var name string = user_result.name
+		var surname string = user_result.surname
+		var email string = user_result.email
+		var age string = user_result.age
+		var phone string = user_result.phone
 
-		user_final, err := collection.InsertOne(ctx, bson.D{
-			{"name", user_result.name},
-			{"surname", user_result.surname},
-			{"email", user_result.email},
-			{"age", user_result.age},
-			{"phone", user_result.phone},
+		user_final, err := usersCollection.InsertOne(ctx, bson.D{
+			{Key: "name", Value: &name},
+			{Key: "surname", Value: &surname},
+			{Key: "email", Value: &email},
+			{Key: "age", Value: &age},
+			{Key: "phone", Value: &phone},
 		})
 
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		//generazione automatica id univoco
 		fmt.Println(user_final.InsertedID)
 		fmt.Println("Complimenti! Hai inserito un nuovo utente!")
 
 	case 2:
-		updateByAge()
+		var mail string = updateUser()
+		var new_name string
+		var new_surname string
+		var new_email string
+		var new_age string
+		var new_phone string
+
+		var update_user User
+
+		if mail != "" && usersCollection.FindOne(ctx, bson.D{{Key: "email", Value: &mail}}) != nil {
+
+			fmt.Println("INSERISCI NOME: ")
+			fmt.Scanln(&new_name)
+			update_user.name = new_name
+
+			fmt.Println("INSERISCI COGNOME: ")
+			fmt.Scanln(&new_surname)
+			update_user.surname = new_surname
+
+			fmt.Println("INSERISCI EMAIL: ")
+			fmt.Scanln(&new_email)
+			update_user.email = new_email
+
+			fmt.Println("INSERISCI ETA': ")
+			fmt.Scanln(&new_age)
+			update_user.age = new_age
+
+			fmt.Println("INSERISCI TELEFONO: ")
+			fmt.Scanln(&new_phone)
+			update_user.phone = new_phone
+			/*
+				usersCollection.ReplaceOne(ctx, bson.D{
+					{Key: "name", Value: new_name},
+					{Key: "surname", Value: new_surname},
+					{Key: "email", Value: new_email},
+					{Key: "age", Value: new_age},
+					{Key: "phone", Value: new_phone},
+				}, bson.D{
+					{Key: "age", Value: &new_age},
+				},
+				)*/
+
+		}
 
 	case 3:
-		updateByPhone()
-
-	case 4:
 		deleteUser()
+
 	}
+
+	defer client.Disconnect(ctx)
 
 }
 
@@ -84,7 +146,7 @@ func insertNewUser() User {
 	fmt.Scanln(&email)
 
 	fmt.Println("ETA': ")
-	var age int
+	var age string
 	fmt.Scanln(&age)
 
 	fmt.Println("NUMERO DI TELEFONO: ")
@@ -104,56 +166,41 @@ func insertNewUser() User {
 }
 
 //modifica età utente
-func updateByAge() {
+/*func updateByAge() string {
 	fmt.Println("QUALE UTENTE INTENDI MODIFICARE? DIGITA LA SUA MAIL")
 	var mail string
 	fmt.Scanln(&mail)
 
 	//if la mail è contenuta nella lista di mail presa tramite query dal db, allora procedi con la modifica di età dell'utente
 
-}
-
+	return mail
+}*/
+/*
 //modifica numero cellulare utente
-func updateByPhone() {
+func updateByPhone() string {
+	fmt.Println("QUALE UTENTE INTENDI MODIFICARE? DIGITA LA SUA MAIL")
+	var phone string
+	fmt.Scanln(&phone)
+
+	//if la mail è contenuta nella lista di mail presa tramite query dal db, allora procedi con la modifica di numero di telefono dell'utente
+	return phone
+}*/
+
+//metodo per modificare utente
+func updateUser() string {
 	fmt.Println("QUALE UTENTE INTENDI MODIFICARE? DIGITA LA SUA MAIL")
 	var mail string
 	fmt.Scanln(&mail)
 
-	//if la mail è contenuta nella lista di mail presa tramite query dal db, allora procedi con la modifica di numero di telefono dell'utente
+	return mail
 }
 
 //elimina utente
-func deleteUser() {
+func deleteUser() string {
 	fmt.Println("QUALE UTENTE INTENDI ELIMINARE? DIGITA LA SUA MAIL")
 	var mail string
 	fmt.Scanln(&mail)
 
 	//if la mail è contenuta nella lista di mail presa tramite query dal db, allora procedi con l'eliminazione dell'utente
-}
-
-//connessione al mongodb
-func connect_toMongoDB() mongo.Collection {
-	/*
-	   Connect to my cluster
-	*/
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017/"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("SEI CONNESSO AL MONGODB")
-	fmt.Println()
-
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(ctx)
-
-	//new database
-	usersDatabase := client.Database("user_db")
-	//new collection
-	usersCollection := usersDatabase.Collection("user_details")
-
-	return *usersCollection
+	return mail
 }
